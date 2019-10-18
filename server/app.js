a9d4cc3225877a62732bdd3192286d70d166fe36
Ricky Aditya <rickyda79@gmail.com>
2019-10-18 10:24:41 +0700

    Initial Commit

--- server/app.js
+++ server/app.js
@@ -1,15 +1,9 @@
-
 const express = require('express');
-
-
 const app = express();
-
-
 
 const server = app.listen(3001, function() {
     console.log('server running on port 3001');
 });
-
 
 const io = require('socket.io')(server);
 
